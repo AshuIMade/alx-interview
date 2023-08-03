a0abbfd7b3da3113b629633b45b007f9f91e2af2
AsheIMade <asheimade@gmail.com>
2023-08-03 11:20:29 -0700

    boxe unlocking method

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Function to unock boxes with keys
+"""
+
+
+def canUnlockAll(boxes):
+    '''
+    n number of locked boxes each box is numbered 
+    sequentially from 0 to n - 1
+    and each box may contain keys to the other boxes.
+    '''
+
+    list_keys = [0]
+    for i in list_keys:
+        for j in boxes[i]:  # recorro cada caja y anado las keys
+            if j < len(boxes) and j not in list_keys:
+                list_keys.append(j)
+        if len(list_keys) == len(boxes):
+            return True
+    return False
