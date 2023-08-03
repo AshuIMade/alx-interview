b121d1fcc8f3d8e5ac39dd78d0e4ac05a73a0590
AsheIMade <asheimade@gmail.com>
2023-08-03 11:31:03 -0700

    correct error

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-Function to unock boxes with keys
+Function to unock boxex
 """
 
 
 def canUnlockAll(boxes):
     '''
-    N number of locked boxes each box is numbered 
-    sequentially from 0 to n - 1
+    Given n number of locked boxes.
+    Each box is numbered sequentially from 0 to n - 1
     and each box may contain keys to the other boxes.
     '''
 
